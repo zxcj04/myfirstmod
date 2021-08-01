06747a7284687fac0f27eba2fb5d0697a951f33e
zxcj04 <zxcj04@gmail.com>
2021-08-01 23:57:15 +0800

    show durability bar with random number

--- src/main/java/com/fanrende/myfirstmod/items/EnergyPickaxe.java
+++ src/main/java/com/fanrende/myfirstmod/items/EnergyPickaxe.java
@@ -4,7 +4,10 @@ import com.fanrende.myfirstmod.setup.ModSetup;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.common.ToolType;
+
+import java.util.Random;
 
 public class EnergyPickaxe extends Item
 {
@@ -12,6 +15,8 @@ public class EnergyPickaxe extends Item
 	{
 		super(new Item.Properties().maxStackSize(1).group(ModSetup.ITEM_GROUP));
 	}
+
+	private static final Random rand = new Random();
 
 	@Override
 	public float getDestroySpeed(ItemStack stack, BlockState state)
@@ -26,5 +31,23 @@ public class EnergyPickaxe extends Item
 			return true;
 		else
 			return false;
+	}
+
+	@Override
+	public boolean showDurabilityBar(ItemStack stack)
+	{
+		return true;
+	}
+
+	@Override
+	public double getDurabilityForDisplay(ItemStack stack)
+	{
+		return rand.nextDouble();
+	}
+
+	@Override
+	public int getRGBDurabilityForDisplay(ItemStack stack)
+	{
+		return MathHelper.rgb(39.6f, 13.7f, 67.8f);
 	}
 }
