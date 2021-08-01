44a1369e7fbdd821808be04d5eae9dc3b5a7acdd
zxcj04 <zxcj04@gmail.com>
2021-08-02 06:03:16 +0800

    energy tag
    Signed-off-by: zxcj04 <zxcj04@gmail.com>

--- src/main/java/com/fanrende/myfirstmod/items/EnergyPickaxe.java
+++ src/main/java/com/fanrende/myfirstmod/items/EnergyPickaxe.java
@@ -1,13 +1,27 @@
 package com.fanrende.myfirstmod.items;
 
 import com.fanrende.myfirstmod.setup.ModSetup;
+import com.fanrende.myfirstmod.tools.CustomEnergyStorage;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.energy.CapabilityEnergy;
 
-import java.util.Random;
+import javax.annotation.Nullable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EnergyPickaxe extends Item
 {
@@ -16,21 +30,28 @@ public class EnergyPickaxe extends Item
 		super(new Item.Properties().maxStackSize(1).group(ModSetup.ITEM_GROUP));
 	}
 
-	private static final Random rand = new Random();
+	@Nullable
+	@Override
+	public ICapabilityProvider initCapabilities(
+			ItemStack stack, @Nullable CompoundNBT nbt
+	)
+	{
+		return new CustomEnergyStorage.Item.Provider(stack, 10000, 1000);
+	}
 
 	@Override
 	public float getDestroySpeed(ItemStack stack, BlockState state)
 	{
-		return 8.0f;
+		return 12.0f;
 	}
 
 	@Override
 	public boolean canHarvestBlock(BlockState blockIn)
 	{
-		if(blockIn.getHarvestTool() == ToolType.PICKAXE)
+		if (blockIn.getHarvestTool() == ToolType.PICKAXE)
 			return true;
 		else
-			return false;
+			return super.canHarvestBlock(blockIn);
 	}
 
 	@Override
@@ -42,12 +63,69 @@ public class EnergyPickaxe extends Item
 	@Override
 	public double getDurabilityForDisplay(ItemStack stack)
 	{
-		return rand.nextDouble();
+		AtomicInteger energyStored = new AtomicInteger(0);
+
+		stack.getCapability(CapabilityEnergy.ENERGY).ifPresent(h -> energyStored.set(h.getEnergyStored()));
+
+		return (double) 1 - ( (double) energyStored.get() / (double) 10000 );
 	}
 
 	@Override
 	public int getRGBDurabilityForDisplay(ItemStack stack)
 	{
-		return MathHelper.rgb(39.6f, 13.7f, 67.8f);
+		return MathHelper.hsvToRGB(0.72F, 0.66F, 1.0F);
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(
+			World worldIn, PlayerEntity playerIn, Hand handIn
+	)
+	{
+		ItemStack stack = playerIn.getHeldItem(handIn);
+
+		stack.getCapability(CapabilityEnergy.ENERGY).ifPresent(h ->
+		{
+			( (CustomEnergyStorage.Item) h ).setEnergy(10000);
+
+			System.out.println(h.getEnergyStored());
+		});
+
+		worldIn.playSound(
+				(PlayerEntity) null,
+				playerIn.getPosition(),
+				SoundEvents.ITEM_CROSSBOW_LOADING_MIDDLE,
+				SoundCategory.NEUTRAL,
+				0.65F,
+				0.4F / ( random.nextFloat() * 0.4F + 0.8F )
+		);
+
+		return super.onItemRightClick(worldIn, playerIn, handIn);
+	}
+
+	@Override
+	public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, PlayerEntity player)
+	{
+		AtomicBoolean stopBreaking = new AtomicBoolean(false);
+
+		stack.getCapability(CapabilityEnergy.ENERGY).ifPresent(h -> stopBreaking.set(h.getEnergyStored() < 1000));
+
+		if (stopBreaking.get())
+			return true;
+
+		return super.onBlockStartBreak(stack, pos, player);
+	}
+
+	@Override
+	public boolean onBlockDestroyed(
+			ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving
+	)
+	{
+		stack.getCapability(CapabilityEnergy.ENERGY).ifPresent(h ->
+		{
+			( (CustomEnergyStorage.Item) h ).extractEnergy(1000, false);
+
+			System.out.println(h.getEnergyStored());
+		});
+		return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
 	}
 }
--- src/main/java/com/fanrende/myfirstmod/tools/CustomEnergyStorage.java
+++ src/main/java/com/fanrende/myfirstmod/tools/CustomEnergyStorage.java
@@ -1,8 +1,17 @@
 package com.fanrende.myfirstmod.tools;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.EnergyStorage;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class CustomEnergyStorage extends EnergyStorage implements INBTSerializable<CompoundNBT>
 {
@@ -48,5 +57,70 @@ public class CustomEnergyStorage extends EnergyStorage implements INBTSerializab
 	public void deserializeNBT(CompoundNBT nbt)
 	{
 		setEnergy(nbt.getInt("energy"));
+	}
+
+	public static class Item extends CustomEnergyStorage
+	{
+		private final ItemStack stack;
+
+		public Item(ItemStack stack, int capacity, int transfer)
+		{
+			super(capacity, transfer);
+			this.stack = stack;
+			setEnergy(this.stack.getOrCreateTag().getInt("energy"));
+		}
+
+		@Override
+		public void setEnergy(int energy)
+		{
+			super.setEnergy(energy);
+
+			this.stack.getOrCreateTag().putInt("energy", getEnergyStored());
+		}
+
+		@Override
+		public int receiveEnergy(int maxReceive, boolean simulate)
+		{
+			int energy = super.receiveEnergy(maxReceive, simulate);
+			if (!simulate)
+			{
+				this.stack.getOrCreateTag().putInt("energy", getEnergyStored());
+			}
+			return energy;
+		}
+
+		@Override
+		public int extractEnergy(int maxExtract, boolean simulate)
+		{
+			int energy = super.extractEnergy(maxExtract, simulate);
+			if (!simulate)
+			{
+				this.stack.getOrCreateTag().putInt("energy", getEnergyStored());
+			}
+			return energy;
+		}
+
+		public static class Provider implements ICapabilityProvider
+		{
+			private final ItemStack stack;
+			private final int capacity;
+			private final int transfer;
+
+			public Provider(ItemStack stack, int capacity, int transfer)
+			{
+				this.stack = stack;
+				this.capacity = capacity;
+				this.transfer = transfer;
+			}
+
+			@Override
+			public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side)
+			{
+				return cap == CapabilityEnergy.ENERGY ? LazyOptional.of(() ->
+				{
+					return new Item(this.stack, this.capacity, this.transfer);
+				}).cast() : LazyOptional.empty();
+			}
+		}
 	}
 }
