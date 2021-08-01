c4dd439ce0f954c9f2703de1e203d81bc3db161a
zxcj04 <zxcj04@gmail.com>
2021-08-01 23:50:50 +0800

    break block like a pickaxe without cost

--- src/main/java/com/fanrende/myfirstmod/items/EnergyPickaxe.java
+++ src/main/java/com/fanrende/myfirstmod/items/EnergyPickaxe.java
@@ -1,12 +1,30 @@
 package com.fanrende.myfirstmod.items;
 
 import com.fanrende.myfirstmod.setup.ModSetup;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.ToolType;
 
 public class EnergyPickaxe extends Item
 {
 	public EnergyPickaxe()
 	{
 		super(new Item.Properties().maxStackSize(1).group(ModSetup.ITEM_GROUP));
+	}
+
+	@Override
+	public float getDestroySpeed(ItemStack stack, BlockState state)
+	{
+		return 8.0f;
+	}
+
+	@Override
+	public boolean canHarvestBlock(BlockState blockIn)
+	{
+		if(blockIn.getHarvestTool() == ToolType.PICKAXE)
+			return true;
+		else
+			return false;
 	}
 }
